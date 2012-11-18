06650acff53e31a654e97cdfe7200647955ea543
Robert Zenz <bobby@bonsaimind.org>
2012-11-18 13:11:02 +0100

    Extracted values into vaiables.

--- Nexoc S621II/CalibrateScreen.sh
+++ Nexoc S621II/CalibrateScreen.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+$basex=4132
+$basey=4135
+
+$x=$basex
+$y=$basey
+
 ids=$(xinput --list | sort -r | grep "HID TOUCH HID Touch Panel" | grep -o -E "id=[0-9]+" | grep -o -E "[0-9]+")
 
 if [ "$ids" != "" ]; then
 	for id in $ids; do
 		echo "TouchScreen found in id: $id"
-		xinput set-prop $id "Evdev Axis Calibration" 0 4132 0 4135
+		xinput set-prop $id "Evdev Axis Calibration" 0 $basex 0 $basey
 	done
 fi
