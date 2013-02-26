32cf549f31358c2c23fc4e01347719b89c301e22
Robert Zenz <Bobby@bonsaimind.org>
2013-02-26 20:41:06 +0100

    Impressive how easy stuff gets ones you read the manual.

--- Desktop/RandomWallpaper.sh
+++ Desktop/RandomWallpaper.sh
@@ -3,44 +3,15 @@
 # This is a script which sets a random wallpaper.
 # Some ideas are coming from the xplanet-script located at: http://rbrusu.com/xplanet-desktop-wallpape.html
 
-# Written by Robert 'Bobby' Zenz (Robert.Zenz@gmx.at or bobby@bonsaimind.org) (http://www.bonsamind.org)
-# Written for UK at Superuser.com (http://superuser.com/questions/97737/software-for-random-wallpaper-but-with-custom-text)
-
 # Public Domain
 
-# This is a stripped down version of RandomQuotedWallpaper.sh
+# Directory of the wallpapers
+wallpaperdir=~/Wallpapers
 
-# Config-Section
-# --------------
-wallpaper=~/wallpapers/			# Set it to a fixed wallpaper, or to a folder to pick
-								# a random one inside that
-#---------------
+if [ ! -d "$wallpaperdir" ]; then
+	echo "$wallpaperdir does not exist!"
+fi
 
+newWallpaper=$(ls "$wallpaperdir" | sort --random-sort | head -n 1)
 
-# Global variable, please ignore this...
-pickedWallpaper=GlobalyDefined
-
-function getRandomFile {
-	cd $1
-
-	set -- *
-	length=$#
-	random_num=$(( $RANDOM % ($length + 1) ))
-
-	pickedWallpaper=${!random_num}
-
-	while [[ ! -e $pickedWallpaper ]]; do
-		pickedWallpaper=${!random_num}
-	done
-
-	pickedWallpaper=$(pwd)/$pickedWallpaper
-
-	cd -
-}
-
-function main {
-	getRandomFile $wallpaper
-	gconftool -t str -s /desktop/gnome/background/picture_filename $pickedWallpaper
-}
-
-main
+gconftool -t str -s /desktop/gnome/background/picture_filename $newWallpaper
