23a192a45149228cd512cf0c28d0d4379bcdc5bc
Robert Zenz <bobby@bonsaimind.org>
2013-03-02 21:04:54 +0100

    Added a parameter, fixed problems with sub-directories.

--- Text/getRandomLine.sh
+++ Text/getRandomLine.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-# Script for getting a random text (3rd version).
+# Script for getting a random line from a random (or defined) file.
 
-# Written by Robert 'Bobby' Zenz (Robert.Zenz@gmx.at or bobby@bonsaimind.org) (http://www.bonsamind.org)
+# Public Domain or CC0
 
+# Default source of comments
 source=~/Documents/Quotes
 
-randomFile=$(ls "$source" | sort --random-sort | head -1)
-randomLine=$(cat "$source/$randomFile" | sort --random-sort | head -1)
+# Check if we got a new source
+if [ ! -z "$1" ]; then
+	source=$1
+fi
 
-echo $randomLine - $randomFile
+if [ ! -e "$source" ]; then
+	echo "$source not found."
+	exit 1
+fi
+
+file=$source
+if [ -d "$source" ]; then
+	file=$(find "$source" -maxdepth 1 -type f | sort --random-sort | head -1)
+fi
+
+line=$(cat "$file" | sort --random-sort | head -1)
+
+echo $line - $(basename "$file")
