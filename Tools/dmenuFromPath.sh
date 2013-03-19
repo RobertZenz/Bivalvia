80be3e6dea92c247d3d0093b4bf02d0c3a07d521
Robert Zenz <Bobby@bonsaimind.org>
2013-03-19 19:21:54 +0100

    Parameters are now passed on.

--- Tools/dmenuFromPath.sh
+++ Tools/dmenuFromPath.sh
@@ -7,5 +7,5 @@
 
 executables=$(echo $PATH | tr -s ':' '\n' | xargs "-I{}" find -L "{}" -type f -executable -maxdepth 1 -printf "%f\n" | sort)
 
-toStart=$(echo "$executables" | dmenu)
+toStart=$(echo "$executables" | dmenu "$@")
 exec $toStart
