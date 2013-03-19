f0cdddcad1a83d2a242e752e8af77094a0c85777
Robert Zenz <Bobby@bonsaimind.org>
2013-03-19 19:18:44 +0100

    Added comments.

--- Tools/dmenuFromPath.sh
+++ Tools/dmenuFromPath.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Launches demu with the contents of the PATH variable.
+# For some reason that does not work on my Ubuntu 12.10.
+
+# Public Domain or CC0
+
 executables=$(echo $PATH | tr -s ':' '\n' | xargs "-I{}" find -L "{}" -type f -executable -maxdepth 1 -printf "%f\n" | sort)
 
 toStart=$(echo "$executables" | dmenu)
