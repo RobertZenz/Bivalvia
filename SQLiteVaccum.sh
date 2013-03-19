652013a8db29f36a21bb88724bf6387791c02523
Robert Zenz <Bobby@bonsaimind.org>
2013-03-19 23:27:30 +0100

    Cleaned up.

--- SQLiteVaccum.sh
+++ SQLiteVaccum.sh
@@ -1,43 +1,25 @@
 #!/bin/bash
 
-# This is a simple script to clean up the
-# Firefox SQL-Lite databases and write
-# all actions to an endless log
-# sqlite3 must be installed for this script.
+# This is a simple script to run the VACUUM command on all
+# SQLite-Databases found in the specified directory.
 
 # Original idea is here: http://www.gettingclever.com/2008/06/vacuum-your-firefox-3.html
 
-# Written by Robert 'Bobby' Zenz (Robert.Zenz@gmx.at or bobby@bonsaimind.org) (http://www.bonsamind.org)
-# Public Domain
+# Public Domain or CC0
 
-# Set the log path
-CONST_LOG=~/.SQLiteVaccum.log
+if [ $(expr length "$(which sqlite3)") -eq 0 ]; then
+	echo "Please install sqlite."
+	exit 1
+fi
 
-CONST_SQLITE_INSTALLED=$(expr length "$(which sqlite3)")
-
-function doClean {
-	toLog "  --- Started cleaning in $1 ---  "
-
-	for item in $1/*.sqlite; do
-		let sizeBefore=$(stat -c%s $item)
-				
-		echo "VACUUM;" | sqlite3 $item
-		toLog "Vaccum: $item, Size from $sizeBefore to $(stat -c%s $item)"
+for directory in "$@"; do
+	for database in $directory/*.sqlite; do
+		before=$(stat -c%s "$database")
+		echo -n "$database: From $before "
+		
+		$(echo "VACUUM;" | sqlite3 "$database")
+		
+		after=$(stat -c%s "$database")
+		echo "$after; $(echo "scale=2;$after*100/$before" | bc)%"
 	done
-}
-
-function toLog {
-	echo $1
-	echo "["$(date)"] $1" >> $CONST_LOG
-}
-
-function main {
-	if [ ! $CONST_SQLITE_INSTALLED -eq 0 ]; then
-		doClean $1
-	else
-		toLog "sqlite3 is not installed..."
-	fi
-}
-
-# Call the main function
-main $1
+done
