a10a7c8ad0b77482f675af762e157903ee8722be
Robert Zenz <Bobby@bonsaimind.org>
2013-03-25 21:25:13 +0100

    Added.

---
+++ Tools/genPassWiki.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Generates random, XKCD-style passwords from Wikipedia
+# genPassWiki.sh
+
+# Public Domain or CC0
+
+# TODO: paste should take an arbitrary amount of input
+
+words=4
+fromSet="a-zA-Z0-9"
+count=1
+
+if [ ! -z "$1" ]; then
+	words=$1
+fi
+
+if [ ! -z "$2" ]; then
+	fromSet=$2
+fi
+
+if [ ! -z "$3" ]; then
+	count=$3
+fi
+
+curl --silent --location http://en.wikipedia.org/wiki/Special:Random | \
+html2text | egrep --only-matching "[$fromSet]*" | sort | uniq | sort --random-sort | \
+head --lines=$(($words*$count)) | paste "--delimiters=" - - - -
+
