1cfd206fa98de699ffb55faa4c7e335374ed4e57
Robert Zenz <Bobby@bonsaimind.org>
2013-04-04 19:38:25 +0200

    Added.

---
+++ Tools/genPassString.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Generates random passwords from /dev/urandom
+# genPass.sh LENGTH COUNT
+
+# Public Domain or CC0
+
+length=8
+count=3
+
+if [ ! -z "$1" ]; then
+	length=$1
+fi
+
+if [ ! -z "$2" ]; then
+	count=$2
+fi
+
+cat /dev/urandom | strings --bytes=$length | head -$count
