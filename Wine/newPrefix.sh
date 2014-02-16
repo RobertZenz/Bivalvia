4e87c19ff1f07c9e37e7d10f4670aec73dd39f11
Robert Zenz <bobby@bonsaimind.org>
2014-02-16 14:58:43 +0100

    Added.

---
+++ Wine/newPrefix.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+
+# Creates a new wine prefix in the current directory
+# and some helper scripts. It also directly starts winecfg and
+# winetricks.
+# Public Domain or CC0
+
+# Usage: newPrefix.sh
+# Just execute it in the directory in which you want the prefix.
+
+
+scriptStart="#!/usr/bin/env sh
+
+. \"\$(dirname \"\$0\")/base.sh\"
+"
+
+createScript() {
+	echo "$2" > "$1"
+	chmod u+x "$1"
+}
+
+
+createScript base.sh "#!/usr/bin/env sh
+
+export WINEPREFIX=\"$(pwd)\"
+"
+
+createScript cfg.sh "$scriptStart
+winecfg
+"
+
+createScript run.sh "$scriptStart
+wine \$*
+"
+
+createScript tricks.sh "$scriptStart
+winetricks \$*
+"
+
+./cfg.sh
+./tricks.sh
+
